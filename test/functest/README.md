79dc8c7fe14ebce964a9aee6779026dac17c4319
jose.lausuch <jose.lausuch@ericsson.com>
2016-06-28 13:35:21 +0200

    Added directory structure for the tests and script templates.
    JIRA: SDNVPN-19
    
    Change-Id: I358efcb377211182a93b2db43f248ca82c14fb36
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ test/functest/README.md
@@ -0,0 +1,5 @@
+The functest directory contains the test cases implementation to be run inside the 
+Functest framework.
+
+The definition of the test cases is explained in the project Wiki page: 
+https://wiki.opnfv.org/display/sdnvpn/main
---
+++ test/functest/testcase_3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2015 All rights reserved
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+
+import functest.utils.functest_logger as ft_logger
+import functest.utils.functest_utils as functest_utils
+import functest.utils.openstack_utils as os_utils
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
