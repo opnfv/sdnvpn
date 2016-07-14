d118ba357ece52c37e03d26ed114ec59f57c2ca5
jose.lausuch <jose.lausuch@ericsson.com>
2016-07-14 10:58:33 +0200

    Add tempest wrapper to enable the bgpvpn plugin for tempest
    The tempest.conf is copied to bgpvpn_tempest.conf to be updated
    with the needed parameters, in this case, only adding
    bgpvpn=True.
    The way to execute this module only is with the option -C.
    For example:
    
    run_tempest.sh -C bgpvpn_tempest.conf -t
      -N -- networking_bgpvpn_tempest
    
    Change-Id: I465961bc619f59355be964eb94f916cb877b2273
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ test/functest/tempest.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2015 All rights reserved
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+import ConfigParser
+import os
+import re
+import shutil
+import functest.utils.functest_logger as ft_logger
+import functest.utils.functest_utils as ft_utils
+
+logger = ft_logger.Logger("bgpvpn").getLogger()
+
+
+def main():
+    src_tempest_dir = ft_utils.get_deployment_dir(logger)
+    if not src_tempest_dir:
+        logger.error("Rally deployment not found.")
+        exit(-1)
+
+    src_tempest_conf = src_tempest_dir + '/tempest.conf'
+    bgpvpn_tempest_conf = src_tempest_dir + '/bgpvpn_tempest.conf'
+
+    if not os.path.isfile(src_tempest_conf):
+        logger.error("tempest.conf not found in %s." % src_tempest_conf)
+        exit(-1)
+    shutil.copy(src_tempest_conf, bgpvpn_tempest_conf)
+
+    logger.info("Copying tempest.conf to %s." % bgpvpn_tempest_conf)
+    config = ConfigParser.RawConfigParser()
+    config.read(bgpvpn_tempest_conf)
+    config.set('service_available', 'bgpvpn', 'True')
+    logger.debug("Updating %s with bgpvpn=True" % bgpvpn_tempest_conf)
+    with open(bgpvpn_tempest_conf, 'wb') as config_file:
+        config.write(config_file)
+
+    cmd_line = (src_tempest_dir +
+                "/run_tempest.sh -C %s -t -N -- "
+                "networking_bgpvpn_tempest" % bgpvpn_tempest_conf)
+    logger.info("Executing: %s" % cmd_line)
+    cmd = os.popen(cmd_line)
+    output = cmd.read()
+    logger.debug(output)
+    # Results parsing
+    error_logs = ""
+    duration = 0
+    failed = 0
+    try:
+        # Look For errors
+        error_logs = ""
+        for match in re.findall('(.*?)[. ]*FAILED', output):
+            error_logs += match
+        # look for duration
+        m = re.search('tests in(.*)sec', output)
+        duration = m.group(1)
+        # Look for num tests run
+        m = re.search('Ran:(.*)tests', output)
+        num_tests = m.group(1)
+        # Look for tests failed
+        m = re.search('Failed:(.*)', output)
+        failed = m.group(1)
+        # Look for name of the tests
+        testcases = re.findall("\{0\} (.*)", output)
+
+        results = {"test_name": "tempest", "duration": duration,
+                   "num_tests": num_tests, "failed": failed,
+                   "tests": testcases}
+        logger.info("Results: %s" % results)
+        return results
+    except:
+        logger.error("Problem when parsing the results.")
+
+
+if __name__ == '__main__':
+    main()
