ec8d50466e46da47e927d53ca55939fdfde42415
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2016-12-14 13:22:48 +0100

    Execute script in lib folder/Use vjumb as Gateway
    Change-Id: I9ad9c9473829fb64ea5ed4f1a21d77aa16615344
    Signed-off-by: Nikolas Hermanns <nikolas.hermanns@ericsson.com>

--- odl-pipeline/lib/odl_reinstaller.sh
+++ odl-pipeline/lib/odl_reinstaller.sh
@@ -3,4 +3,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 set -e
 export PYTHONPATH=$PYTHONPATH:$DIR
 mkdir -p $DIR/tmp
-python $DIR/odl_reinstaller/odl_reinstaller.py $@
+cd $DIR
+python ./odl_reinstaller/odl_reinstaller.py $@
