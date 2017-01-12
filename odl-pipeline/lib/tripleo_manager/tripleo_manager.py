647978a8e2485b4757def1dff3823fc80e25da3f
Tim Rozet <trozet@redhat.com>
2017-01-12 12:27:41 -0500

    Updates ODL Pipeline scripts for CSIT
    Changes Include:
     - TripleOManager split into two purposes.  One is to introspect a
       current Apex deployment and dump yaml config to be bundled with
       snapshots.  Second purpose is to handle re-installing ODL from a
       given tarball artifact on a snapshot setup.
     - New argument for passing the SSH private key to use to connect to
       nodes.
     - Some general clean up and consolidation of logic
    
    JIRA: APEX-363
    
    Change-Id: I792db0fac3f4e81969fe85c05fc298fe5af02537
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- odl-pipeline/lib/tripleo_manager/tripleo_manager.py
+++ odl-pipeline/lib/tripleo_manager/tripleo_manager.py
@@ -1,179 +1,218 @@
 import os
+import commands
+import re
+import time
 import yaml
-from novaclient.client import Client as nova
-from novaclient import api_versions
-import ironicclient.client
-from neutronclient.v2_0.client import Client as neutron
-from keystoneauth1 import identity
-from keystoneauth1 import session
 
+from utils import processutils
+from odl_reinstaller.odl_reinstaller import ODLReInstaller
+from utils.node import Node
 from utils.utils_log import log_enter_exit, for_all_methods, LOG
 from utils.service import Service
 from utils.shutil import shutil
 from utils.node_manager import NodeManager
+from utils.ssh_util import SshUtil
 from common import config as CONFIG
+from deployment_cloner.deployment_cloner import DeploymentCloner
 
 
 @for_all_methods(log_enter_exit)
 class TripleOManager(Service):
 
     def __init__(self):
-        self.auth = None
-        self.session = None
-        self.novacl = self._get_nova_client()
-        self.ironiccl = self._get_ironic_client()
-        self.neutroncl = self._get_neutron_client()
+        self.node_info = {'servers': {}}
+        self.overcloud_user = 'heat-admin'
+        self.netvirt_url = "restconf/operational/network-topology:" \
+                           "network-topology/topology/netvirt:1"
 
     def create_cli_parser(self, parser):
-        parser.add_argument('--out', help="where env_info should go to",
-                            required=True)
+        parser.add_argument('--introspect',
+                            help="Gather node info and dump info",
+                            default=False,
+                            action='store_true')
+        parser.add_argument('--update-opendaylight',
+                            help="Remove current OpenDaylight and disconnect"
+                                 "OVS instances, then reinstall ODL",
+                            default=False,
+                            action='store_true',
+                            dest='update_odl')
+        parser.add_argument('--out',
+                            help="Directory where env_info will be written to",
+                            default=os.getcwd(),
+                            required=False)
+        parser.add_argument('--pod-config',
+                            help="Config file when cleaning the deployment",
+                            default=None,
+                            required=False,
+                            dest='pod_config')
+        parser.add_argument('--artifact',
+                            help="OpenDaylight Tarball to be installed when "
+                                 "--update-opendaylight is used",
+                            default=None,
+                            required=False)
+        parser.add_argument('--ssh-key-file',
+                            help="SSH private key file to use when updating "
+                                 "ODL on new snapshot deployment",
+                            default=SshUtil.get_id_rsa(),
+                            required=False,
+                            dest='ssh_key_file')
         return parser
 
     def run(self, sys_args, config):
-        self.gen_node_info()
-        self.prepare_for_ci_pipeline()
-        self.gen_env_info(sys_args)
+        if sys_args.introspect is True:
+            self.gen_node_info()
+            self.gen_env_info(sys_args.out)
+        if sys_args.update_odl is True:
+            if sys_args.pod_config is not False:
+                self.load_config(sys_args.pod_config)
+            elif not bool(self.node_info['servers']):
+                raise TripleOManagerException("No config found for cleaning "
+                                              "deployment.  Please "
+                                              "provide config via --config "
+                                              "argument")
+            if sys_args.artifact is None:
+                raise TripleOManagerException("ODL Tarball must be provided "
+                                              "as --artifact when updating "
+                                              "ODL")
+            self.check_node_config()
+            self.prepare_for_ci_pipeline(sys_args.artifact,
+                                         ssh_key_file=sys_args.ssh_key_file)
 
-    def prepare_for_ci_pipeline(self):
-        node_manager = NodeManager(config=self.node_info['servers'])
+    def prepare_for_ci_pipeline(self, odl_tarball, ssh_key_file=None):
+        node_manager = NodeManager(config=self.node_info['servers'],
+                                   ssh_key_file=ssh_key_file)
+        odl_node = None
+        # Clean All Nodes
+        LOG.info("Removing previous OpenDaylight and disconnecting OVS")
         for node in node_manager.get_nodes():
-
-            # Check is ODL runs on this node
-            self.node_info['servers'][node.name]['ODL'] = {}
+            # Check if ODL runs on this node
             rv, _ = node.execute('ps aux |grep -v grep |grep karaf',
                                  as_root=True, check_exit_code=[0, 1])
             if 'java' in rv:
-                self.node_info['servers'][node.name]['ODL']['active'] = True
-
-            if (node.is_dir('/opt/opendaylight') or
-                    node.is_file('/opt/opendaylight-was-there')):
-                self.node_info['servers'][node.name]['ODL']['dir_exsist'] = \
-                    True
-                # Remove existing ODL version
-                node.execute('touch /opt/opendaylight-was-there', as_root=True)
-                node.execute('rm -rf /opt/opendaylight', as_root=True)
-
-            # Store ovs controller info
-            rv, _ = node.execute('ovs-vsctl get-controller br-int',
-                                 as_root=True)
-            self.node_info['servers'][node.name]['ovs-controller'] = \
-                rv.replace('\n', '')
+                odl_node = node
+                node.execute('systemctl stop opendaylight', as_root=True)
 
             # Disconnect ovs
             node.execute('ovs-vsctl del-controller br-int', as_root=True)
+            node.execute('ovs-vsctl del-manager')
 
-    def gen_env_info(self, sys_args):
-        shutil.mkdir_if_not_exsist(sys_args.out)
+        # Bring ODL up back
+        LOG.info("Installing new OpenDaylight")
+        ODLReInstaller.reinstall_odl(odl_node, odl_tarball)
+
+        # Wait for ODL to come back up
+        full_netvirt_url = "http://{}:8081/{}".format(odl_node.config.get(
+            'address'), self.netvirt_url)
+        counter = 1
+        while counter < 10:
+            try:
+                odl_node.execute("curl --fail -u admin:admin {}".format(
+                    full_netvirt_url))
+                LOG.info("New OpenDaylight NetVirt is Up")
+                break
+            except processutils.ProcessExecutionError:
+                LOG.warning("NetVirt not up. Attempt: {}".format(counter))
+                if counter >= 10:
+                    LOG.warning("NetVirt not detected as up after 10 "
+                                "attempts...deployment may be unstable!")
+            counter += 1
+            time.sleep(10)
+
+        # Reconnect OVS instances
+        LOG.info("Reconnecting OVS instances")
+        for node in node_manager.get_nodes():
+            ODLReInstaller.reconnect_ovs(node)
+
+    def gen_env_info(self, output_dir=None):
+        shutil.mkdir_if_not_exist(output_dir)
         self.write_out_yaml_config(self.node_info,
-                                   sys_args.out + CONFIG.NODE_YAML_PATH)
+                                   output_dir + CONFIG.NODE_YAML_PATH)
 
-        # copy ssh key
-        shutil.copy('to', '/home/stack/.ssh/id_rsa',
-                    sys_args.out + CONFIG.ID_RSA_PATH)
-        shutil.copy('to', '/home/stack/.ssh/id_rsa.pub',
-                    sys_args.out + CONFIG.ID_RSA_PATH)
-        # copy rc files
-        shutil.copy('to', '/home/stack/stackrc',
-                    sys_args.out)
-        shutil.copy('to', '/home/stack/overcloudrc',
-                    sys_args.out + CONFIG.OVERCLOUDRC_PATH)
+    def load_config(self, cfg):
+        with open(cfg, 'r') as f:
+            self.node_info = yaml.safe_load(f)
+
+    @staticmethod
+    def find_overcloud_ips():
+        ret, res = commands.getstatusoutput('opnfv-util undercloud '
+                                            'stack "source '
+                                            '/home/stack/stackrc; nova list"')
+        if ret != 0:
+            raise TripleOManagerException("Error unable to issue nova list "
+                                          "on undercloud.  Please verify "
+                                          "undercloud is up")
+        else:
+            return re.findall('ctlplane=([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)', res)
+
+    @staticmethod
+    def introspect_node(node, cmd):
+        try:
+            stdout, (stderr, rc) = node.execute(cmd)
+        except processutils.ProcessExecutionError as e:
+            raise TripleOManagerException("Error while executing {} on node "
+                                          "{}: {}".format(cmd, node.address,
+                                                          e.message))
+        return stdout.rstrip()
 
     def gen_node_info(self):
-        for network in self.neutroncl.list_networks()['networks']:
-            if network['name'] == 'ctlplane':
-                ctlplane_id = network['id']
-        if hasattr(self, 'node_info') and self.node_info:
-            return self.node_info
-        self.node_info = {'servers': {}}
-        for server in self.novacl.servers.list():
-            if 'overcloud-controller' in server.name:
-                type = 'controller'
-            elif 'overcloud-novacompute' in server.name:
-                type = 'compute'
-            else:
-                raise Exception('Unknown type (controller/compute) %s '
-                                % server.name)
-            ctlplane_mac = None
-            for interface in server.interface_list():
-                if interface.net_id == ctlplane_id:
-                    ctlplane_mac = interface.mac_addr
-            if not ctlplane_mac:
-                raise Exception('Could not find mac address for ctl-plane for '
-                                'server %s' % server.name)
-            self.node_info['servers'][server.name] = {
-                'address': self.get_address_of_node(server=server),
-                'user': 'heat-admin',
-                'type': type,
-                'orig-ctl-mac': ctlplane_mac}
+        overcloud_ip_list = TripleOManager.find_overcloud_ips()
 
-    def write_out_yaml_config(self, config, path):
+        for node_ip in overcloud_ip_list:
+            node = Node('intro-%s' % node_ip, address=node_ip,
+                        user=self.overcloud_user)
+            node_mac = None
+            virsh_domain = None
+            server_name = self.introspect_node(node, 'hostname')
+            if 'overcloud-controller' in server_name:
+                node_type = 'controller'
+            elif 'overcloud-novacompute' in server_name:
+                node_type = 'compute'
+            else:
+                raise TripleOManagerException('Unknown type '
+                                              '(controller/compute) %s '
+                                              % server_name)
+            try:
+                commands.getstatusoutput('ping -c 1 %s' % node_ip)
+                ret, res = commands.getstatusoutput('/usr/sbin/arp -a '
+                                                    '%s' % node_ip)
+                node_mac = \
+                    re.search('([0-9a-z]+:){5}[0-9a-z]+', res).group(0)
+                virsh_domain = \
+                    DeploymentCloner().get_virtual_node_name_from_mac(node_mac)
+            except AttributeError:
+                LOG.warning("Unable to find MAC address for node {"
+                            "}".format(node_ip))
+
+            # find ovs controller and manager
+            ovs_controller = self.introspect_node(
+                node, 'sudo ovs-vsctl get-controller br-int')
+            ovs_managers = self.introspect_node(
+                node, 'sudo ovs-vsctl get-manager').split("\n")
+            self.node_info['servers'][server_name] = {
+                 'address': node_ip,
+                 'user': self.overcloud_user,
+                 'type': node_type,
+                 'orig-ctl-mac': node_mac,
+                 'vNode-name': virsh_domain,
+                 'ovs-controller': ovs_controller,
+                 'ovs-managers': ovs_managers}
+
+    @staticmethod
+    def write_out_yaml_config(config, path):
         with open(path, 'w') as f:
             yaml.dump(config, f, default_flow_style=False)
 
-    def _check_credentials(self):
-        for cred in ['OS_USERNAME', 'OS_PASSWORD',
-                     'OS_TENANT_NAME', 'OS_AUTH_URL']:
-            if not os.environ.get(cred):
-                raise Exception('Use export %s=...' % cred)
 
-    def create_auth(self):
-        self._check_credentials()
-        if not self.auth:
-            self.auth = identity.Password(
-                auth_url=os.environ['OS_AUTH_URL'],
-                username=os.environ['OS_USERNAME'],
-                password=os.environ['OS_PASSWORD'],
-                project_name=os.environ['OS_TENANT_NAME'])
-        if not self.session:
-            self.session = session.Session(auth=self.auth)
+class TripleOManagerException(Exception):
+    def __init__(self, value):
+        self.value = value
 
-    def _get_nova_client(self):
-        self.create_auth()
-        return nova(api_versions.APIVersion("2.0"), session=self.session)
-
-    def _get_ironic_client(self):
-        self.create_auth()
-        return ironicclient.client.get_client(1, session=self.session)
-
-    def _get_neutron_client(self):
-        self.create_auth()
-        return neutron(session=self.session)
-
-    def get_node_name_by_ilo_address(self, ilo_address):
-        try:
-            node_name = None
-            for node in self.ironiccl.node.list():
-                nova_uuid = node.instance_uuid
-                if ilo_address == self.ironiccl.node.get_by_instance_uuid(
-                        nova_uuid).driver_info['ilo_address']:
-                    node_name = self.novacl.servers.find(id=nova_uuid).name
-                    break
-            if not node_name:
-                raise Exception('Cannot get nova instance for ilo address %s'
-                                % ilo_address)
-            return node_name
-        except Exception as ex:
-            LOG.error('Unsupported installer platform.')
-            raise ex
-
-    def get_address_of_node(self, server_name=None, server=None):
-        if not (server_name or server):
-            raise Exception('Either server_name or server needs to be given')
-        if server_name:
-            try:
-                for server in self.novacl.servers.list():
-                    if server.name == server_name:
-                        return server.addresses['ctlplane'][0]['addr']
-            except Exception as ex:
-                LOG.error('Unsupported installer platform.')
-                raise ex
-        if server:
-            return server.addresses['ctlplane'][0]['addr']
+    def __str__(self):
+        return self.value
 
 
 def main():
-    main = TripleOManager()
-    main.start()
+    TripleOManager().start()
 
 if __name__ == '__main__':
     main()
--- odl-pipeline/lib/utils/node_manager.py
+++ odl-pipeline/lib/utils/node_manager.py
@@ -16,16 +16,19 @@ class NodeManager(object):
     env_node_dict = {}
     primary_controller = None
 
-    def __init__(self, config=None):
-        if config:
+    def __init__(self, config=None, ssh_key_file=None):
+        self.ssh_key_file = ssh_key_file
+        if config is not None:
             for (node_name, node_config) in config.iteritems():
                 self.add_node(node_name, node_config)
 
     def add_node(self, node_name, node_config):
         from node import Node
         if not node_config.get('address'):
-            node_config['address'] = self.get_address_of_node(node_name)
-        node = Node(node_name, dict=node_config)
+            raise NodeManagerException("IP address missing from node_config:"
+                                       " {}".format(node_config))
+        node = Node(node_name, dict=node_config,
+                    ssh_key_file=self.ssh_key_file)
         self.env_nodes.append(node)
         self.env_node_dict[node_name] = node
         return node
@@ -41,3 +44,8 @@ class NodeManager(object):
         if node not in cls.env_nodes:
             cls.env_nodes.append(node)
         SshUtil.gen_ssh_config(cls.env_nodes)
+
+
+class NodeManagerException(Exception):
+    def __init__(self, value):
+        self.value = value
