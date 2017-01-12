5189c4800ea1480a5f6097cf5dd918ffd2d179e4
Tim Rozet <trozet@redhat.com>
2017-01-12 12:27:41 -0500

    Updates ODL Pipeline scripts for CSIT
    Changes Include:
     - TripleOManager split into two purposes.  One is to introspect a
       current Apex deployment and dump yaml config to be bundled with
       snapshots.  Second purpose is to handle re-installing ODL from a
       given tarball artifact on a snapshot setup.
     - New argument for passing the SSH private key to use to connect to
       nodes.
     - Some general clean up and consolidation of logic
    
    JIRA: APEX-363
    
    Change-Id: I792db0fac3f4e81969fe85c05fc298fe5af02537
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- odl-pipeline/lib/deployment_cloner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-set -e
-export PYTHONPATH=$PYTHONPATH:$DIR
-mkdir -p $DIR/tmp
-cd $DIR
-python ./deployment_cloner/deployment_cloner.py $@
