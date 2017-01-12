87cdeedf3f1ddfb398a68ef9147598b87ef9a2c7
Tim Rozet <trozet@redhat.com>
2017-01-12 12:27:41 -0500

    Updates ODL Pipeline scripts for CSIT
    Changes Include:
     - Change to TripleOInspector which only introspect a
       current Apex deployment and dump yaml config to be bundled with
       snapshots.
     - Add TripleOHelper which consists of all tripleO helper functions.
       Many this are done to virsh so the idea is to have at.
       Some point in time a libvirtHelper, or use another libvirt
       python lib. Thatsway it is a class so that we can inherit
       later on.
     - New argument for passing the SSH private key to use to connect to
       nodes is added to the service utils.
     - Some general clean up and consolidation of logic
    
    JIRA: APEX-363
    
    Change-Id: I792db0fac3f4e81969fe85c05fc298fe5af02537
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- odl-pipeline/lib/odl_reinstaller/odl_reinstaller.py
+++ odl-pipeline/lib/odl_reinstaller/odl_reinstaller.py
@@ -1,56 +1,90 @@
 #!/bin/python
-import os
+import time
+
+from utils import processutils
 from utils.utils_log import LOG, for_all_methods, log_enter_exit
 from utils.service import Service
 from utils.node_manager import NodeManager
-from utils.ssh_util import SSH_CONFIG
-from common import config as CONFIG
+from common import constants
 from utils import utils_yaml
 
 
 @for_all_methods(log_enter_exit)
 class ODLReInstaller(Service):
+    def __init__(self):
+        self.netvirt_url = "restconf/operational/network-topology:" \
+                           "network-topology/topology/netvirt:1"
 
     def run(self, sys_args, config):
-        cloner_info_path = sys_args.cloner_info
-        SSH_CONFIG['ID_RSA_PATH'] = (cloner_info_path + CONFIG.ID_RSA_PATH +
-                                     'id_rsa')
+        pod_config = sys_args.pod_config
         node_config = utils_yaml.read_dict_from_yaml(
-            cloner_info_path + CONFIG.NODE_YAML_PATH)
+            pod_config + constants.NODE_YAML_PATH)
+        # TODO Add validation of incoming node config
+        # self.check_node_config()
+
         # copy ODL to all nodes where it need to be copied
         self.nodes = NodeManager(node_config['servers']).get_nodes()
         for node in self.nodes:
-            LOG.info('Disconnecting OpenVSwitch from controller on node %s'
-                     % node.name)
             node.execute('ovs-vsctl del-controller br-int', as_root=True)
-
+        odl_node = None
         for node in self.nodes:
-            if 'ODL' in node.config:
-                tar_tmp_path = '/tmp/odl-artifact/'
-                if node.config['ODL'].get('active'):
-                    tarball_name = os.path.basename(sys_args.odl_artifact)
-                    node.copy('to', sys_args.odl_artifact,
-                              '/tmp/odl-artifact/' + tarball_name)
-                    node.execute('rm -rf /opt/opendaylight/*', as_root=True)
-                    node.execute('mkdir -p /opt/opendaylight/*', as_root=True)
-                    LOG.info('Extracting %s to /opt/opendaylight/ on node %s'
-                             % (tarball_name, node.name))
-                    node.execute('tar -zxf %s --strip-components=1 -C '
-                                 '/opt/opendaylight/'
-                                 % (tar_tmp_path + tarball_name), as_root=True)
-                    node.execute('chown -R odl:odl /opt/opendaylight',
-                                 as_root=True)
-                    node.execute('rm -rf ' + tar_tmp_path, as_root=True)
-                    LOG.info('Installing and Starting Opendaylight on node %s'
-                             % node.name)
-                    node.copy('to', 'odl_reinstaller/install_odl.pp',
-                              tar_tmp_path)
-                    node.execute('puppet apply --modulepath='
-                                 '/etc/puppet/modules/ %sinstall_odl.pp '
-                                 '--verbose --debug --trace '
-                                 '--detailed-exitcodes'
-                                 % tar_tmp_path, check_exit_code=[2],
-                                 as_root=True)
+            # Check if ODL runs on this node
+            rv, _ = node.execute('ps aux |grep -v grep |grep karaf',
+                                 as_root=True, check_exit_code=[0, 1])
+            if 'java' in rv:
+                odl_node = node
+                node.execute('systemctl stop opendaylight', as_root=True)
+
+            ODLReInstaller.disconnect_ovs(node)
+
+        # Bring ODL up back
+        LOG.info("Installing new OpenDaylight")
+        if odl_node is None:
+            LOG.error("Unable to locate node running OpenDaylight for upgrade")
+            raise ODLReinstallertException("Unable to find node running ODL")
+        ODLReInstaller.reinstall_odl(odl_node, sys_args.odl_artifact)
+
+        # Wait for ODL to come back up
+        full_netvirt_url = "http://{}:8081/{}".format(
+            odl_node.config['address'], self.netvirt_url)
+        counter = 1
+        while counter < 10:
+            try:
+                odl_node.execute("curl --fail -u admin:admin {}".format(
+                    full_netvirt_url))
+                LOG.info("New OpenDaylight NetVirt is Up")
+                break
+            except processutils.ProcessExecutionError:
+                LOG.warning("NetVirt not up. Attempt: {}".format(counter))
+                if counter >= 10:
+                    LOG.warning("NetVirt not detected as up after 10 "
+                                "attempts...deployment may be unstable!")
+            counter += 1
+            time.sleep(10)
+
+        # Reconnect OVS instances
+        LOG.info("Reconnecting OVS instances")
+        for node in self.nodes:
+            ODLReInstaller.connect_ovs(node)
+
+    @staticmethod
+    def reinstall_odl(node, odl_tarball):
+        tar_tmp_path = '/tmp/odl-artifact/'
+        node.copy('to', odl_tarball, tar_tmp_path + odl_tarball)
+        node.execute('rm -rf /opt/opendaylight/*', as_root=True)
+        node.execute('mkdir -p /opt/opendaylight/*', as_root=True)
+        LOG.info('Extracting %s to /opt/opendaylight/ on node %s'
+                 % (odl_tarball, node.name))
+        node.execute('tar -zxf %s --strip-components=1 -C '
+                     '/opt/opendaylight/'
+                     % (tar_tmp_path + odl_tarball), as_root=True)
+        node.execute('chown -R odl:odl /opt/opendaylight', as_root=True)
+        node.execute('rm -rf ' + tar_tmp_path, as_root=True)
+        LOG.info('Installing and Starting Opendaylight on node %s' % node.name)
+        node.execute('puppet apply -e "include opendaylight" '
+                     '--modulepath=/etc/puppet/modules/ '
+                     '--verbose --debug --trace --detailed-exitcodes',
+                     check_exit_code=[2], as_root=True)
         # --detailed-exitcodes: Provide extra information about the run via
         # exit codes. If enabled, 'puppet apply' will use the following exit
         # codes:
@@ -61,22 +95,39 @@ class ODLReInstaller(Service):
         # 4: The run succeeded, and some resources failed.
         # 6: The run succeeded, and included both changes and failures.
 
-        for node in self.nodes:
-            LOG.info('Connecting OpenVSwitch to controller on node %s'
-                     % node.name)
-            ovs_controller = node.config.get('ovs-controller')
-            if ovs_controller:
-                node.execute('ovs-vsctl set-controller br-int %s'
-                             % ovs_controller, as_root=True)
+    @staticmethod
+    def disconnect_ovs(node):
+        LOG.info('Disconnecting OpenVSwitch from controller on node %s'
+                 % node.name)
+        node.execute('ovs-vsctl del-controller br-int', as_root=True)
+        node.execute('ovs-vsctl del-manager', as_root=True)
+
+    @staticmethod
+    def connect_ovs(node):
+        LOG.info('Connecting OpenVSwitch to controller on node %s' % node.name)
+        ovs_controller = node.config['ovs-controller']
+        node.execute('ovs-vsctl set-controller br-int %s'
+                     % ovs_controller, as_root=True)
+        ovs_manager_str = ' '.join(node.config['ovs-managers'])
+        node.execute('ovs-vsctl set-manager %s' % ovs_manager_str,
+                     as_root=True)
 
     def create_cli_parser(self, parser):
-        parser.add_argument('--cloner-info',
+        parser.add_argument('--pod_config',
                             help=("Give the path to the clone info"),
                             required=True)
         parser.add_argument('--odl-artifact',
                             help=("Path to Opendaylight tarball"),
                             required=True)
         return parser
+
+
+class ODLReinstallertException(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
 
 
 def main():
---
+++ odl-pipeline/lib/tripleo_introspector/tripleo_introspector.py
@@ -0,0 +1,98 @@
+import re
+
+from utils import processutils
+from utils.node import Node
+from utils.utils_log import log_enter_exit, for_all_methods, LOG
+from utils.service import Service
+from utils.shutil import shutil
+from common import constants
+from utils import utils_yaml
+from utils.tripleo_helper import TripleoHelper
+
+
+@for_all_methods(log_enter_exit)
+class TripleOIntrospector(Service):
+
+    def __init__(self):
+        self.overcloud_user = 'heat-admin'
+
+    def create_cli_parser(self, parser):
+        parser.add_argument('--out',
+                            help="Directory where env_info will be written to",
+                            required=True)
+        return parser
+
+    def run(self, sys_args, config):
+        dest_dir = sys_args.out if sys_args.out[:-1] == '/'\
+            else (sys_args.out + '/')
+        self.gen_node_info()
+        shutil.mkdir_if_not_exist(dest_dir)
+        utils_yaml.write_dict_to_yaml(self.node_info,
+                                      sys_args + constants.NODE_YAML_PATH)
+
+    def gen_node_info(self):
+        overcloud_ip_list = TripleoHelper.find_overcloud_ips()
+
+        for node_ip in overcloud_ip_list:
+            LOG.info('Introspecting node %s' % node_ip)
+            node = Node('intro-%s' % node_ip, address=node_ip,
+                        user=self.overcloud_user)
+            node_mac = None
+            virsh_domain = None
+            server_name, _ = node.execute('hostname')
+            if 'overcloud-controller' in server_name:
+                node_type = 'controller'
+            elif 'overcloud-novacompute' in server_name:
+                node_type = 'compute'
+            else:
+                raise TripleOInspectorException('Unknown type '
+                                                '(controller/compute) %s '
+                                                % server_name)
+            try:
+                processutils.execute('ping -c 1 %s' % node_ip)
+                res, _ = processutils.execute('/usr/sbin/arp -a '
+                                              '%s' % node_ip)
+                node_mac = \
+                    re.search('([0-9a-z]+:){5}[0-9a-z]+', res).group(0)
+                virsh_domain = \
+                    TripleoHelper.get_virtual_node_name_from_mac(node_mac)
+            except AttributeError:
+                LOG.warning("Unable to find MAC address for node {"
+                            "}".format(node_ip))
+
+            # find ovs controller and manager
+            ovs_controller, _ = node.execute('ovs-vsctl get-controller br-int')
+            out, _ = node.execute('ovs-vsctl get-manager')
+            ovs_managers = out.split("\n")
+            self.node_info['servers'][server_name] = {
+                'address': node_ip,
+                'user': self.overcloud_user,
+                'type': node_type,
+                'orig-ctl-mac': node_mac,
+                'vNode-name': virsh_domain,
+                'ovs-controller': ovs_controller,
+                'ovs-managers': ovs_managers}
+
+    @staticmethod
+    def copy_ssh_id_and_overcloudrc(dest):
+        underloud = TripleoHelper.get_underlcoud()
+        # copy overcloudrc
+        underloud.copy('from', dest, './overcloudrc')
+
+        # copy ssh id
+        underloud.copy('from', dest, '.ssh/id_rsa')
+
+
+class TripleOInspectorException(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+def main():
+    TripleOIntrospector().start()
+
+if __name__ == '__main__':
+    main()
