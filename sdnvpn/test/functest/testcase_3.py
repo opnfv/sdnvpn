0909b97d488a25f492c65b423241b760bbc42c13
tomsou <soth@intracom-telecom.com>
2017-02-16 14:48:09 +0000

    WIP Implement testcase 3: Quagga-ODL integration
    A testcase that verifies OpenDaylight can start and communicate with zrpcd/Quagga.
    
    - Verify that zrpdc service is running
    - Issue bgp speaker start to OpenDaylight
    - Verify that zrpcd has started bgpd
    
    JIRA: SDNVPN-98
    
    Change-Id: Ia0c497292a724161259c669425229d189a317ba2
    Signed-off-by: tomsou <soth@intracom-telecom.com>
    Signed-off-by: Romanos Skiadas <rski@intracom-telecom.com>

--- sdnvpn/test/functest/testcase_3.py
+++ sdnvpn/test/functest/testcase_3.py
@@ -8,10 +8,95 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 #
 
+import argparse
+import functest.utils.functest_logger as ft_logger
+from sdnvpn.lib import config as sdnvpn_config
+from sdnvpn.lib.results import Results
+from opnfv.deployment.factory import Factory as DeploymentFactory
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-r", "--report",
+                    help="Create json result file",
+                    action="store_true")
+
+args = parser.parse_args()
+
+logger = ft_logger.Logger("sdnvpn-testcase-3").getLogger()
+
+COMMON_CONFIG = sdnvpn_config.CommonConfig()
+TESTCASE_CONFIG = sdnvpn_config.TestcaseConfig("testcase_3")
+
 
 def main():
-    pass
+    results = Results(COMMON_CONFIG.line_length)
+    results.add_to_summary(0, "=")
+    results.add_to_summary(2, "STATUS", "SUBTEST")
+    results.add_to_summary(0, "=")
 
+    # TODO unhardcode this to work with apex
+    deploymentHandler = DeploymentFactory.get_handler(
+        'fuel',
+        '10.20.0.2',
+        'root',
+        'r00tme')
+
+    openstack_nodes = deploymentHandler.get_nodes()
+
+    controllers = [node for node in openstack_nodes
+                   if node.is_controller()]
+
+    msg = ("Verify that OpenDaylight can start/communicate with zrpcd/Quagga")
+    results.record_action(msg)
+    results.add_to_summary(0, "-")
+
+    for controller in controllers:
+        logger.info("Starting bgp speaker of controller at IP %s "
+                    % controller.ip)
+        msg = ("zrpd daemon running")
+        logger.info("Checking if zrpc daemon is "
+                    "runnning on the controller node")
+
+        cmd = "systemctl status zrpcd"
+        output = controller.run_cmd(cmd)
+
+        if not output:
+            logger.info("zrpc daemon is not runnning on the controller node")
+            results.add_failure(msg)
+        else:
+            logger.info("zrpc daemon is runnning on the controller node")
+            results.add_success(msg)
+
+        results.add_to_summary(0, "-")
+
+        # TODO here we need the external ip of the controller
+        cmd_start_quagga = '/opt/opendaylight/bin/client "odl:configure-bgp ' \
+                           '-op start-bgp-server --as-num 100 ' \
+                           '--router-id {0}"'.format(controller.ip)
+
+        # output_start_quagga =
+        controller.run_cmd(cmd_start_quagga)
+
+        msg = ("bgpd daemon running")
+        logger.info("Checking if bgpd daemon is runnning"
+                    " on the controller node")
+
+        # Check if there is a non-zombie bgpd process
+        output_bgpd = controller.run_cmd("ps --no-headers -C bgpd -o state")
+        states = output_bgpd.split()
+        running = any([s != 'Z' for s in states])
+
+        if not running:
+            logger.info("bgpd daemon is not runnning on the controller node")
+            results.add_failure(msg)
+        else:
+            logger.info("bgpd daemon is runnning on the controller node")
+            results.add_success(msg)
+
+        results.add_to_summary(0, "-")
+
+        # TODO stop bgpd
+        return results.compile_summary()
 
 if __name__ == '__main__':
     main()
