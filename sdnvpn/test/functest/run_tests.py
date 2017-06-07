7acfb0e90e9adeceebbd7cbc8055862b8deb374c
jose.lausuch <jose.lausuch@ericsson.com>
2017-06-07 16:12:41 +0200

    Adapt SDNVPN Functest test according to new framework
    - Inherit from TestCase class in Functest
    - Remove any push to DB method, since Functest
       takes care of it
    
    After this change, we will be able to load this module
    from Functest and make a python call instead of
    a bash call.
    
    Change-Id: I80bdb7eaac3a2d1f48e32bafa381a42cadb36138
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

--- sdnvpn/test/functest/run_tests.py
+++ sdnvpn/test/functest/run_tests.py
@@ -8,7 +8,6 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 #
 
-import argparse
 import importlib
 import logging
 import os
@@ -17,93 +16,88 @@ import time
 import traceback
 import yaml
 
-import functest.utils.functest_utils as ft_utils
+from functest.core import testcase
 from sdnvpn.lib import config as sdnvpn_config
 
+""" logging configuration """
+logger = logging.getLogger(__name__)
 
-logger = logging.getLogger('sdnvpn-run-tests')
 
 COMMON_CONFIG = sdnvpn_config.CommonConfig()
-TEST_DB_URL = COMMON_CONFIG.test_db
 
 
-def push_results(testname, start_time, end_time, criteria, details):
-    logger.info("Push testcase '%s' results into the DB...\n" % testname)
-    ft_utils.push_results_to_db("sdnvpn",
-                                testname,
-                                start_time,
-                                end_time,
-                                criteria,
-                                details)
+class SdnvpnFunctest(testcase.TestCase):
 
+    def run(self):
+        self.start_time = time.time()
 
-def main(report=False):
-    # Workaround for https://jira.opnfv.org/projects/SDNVPN/issues/SDNVPN-100
-    # and SDNVPN-126
-    cmd_line = "neutron quota-update --subnet -1 --network -1 --port -1"
-    logger.info("Setting subnet/net quota to unlimited : %s" % cmd_line)
-    cmd = os.popen(cmd_line)
-    output = cmd.read()
-    logger.debug(output)
+        cmd_line = "neutron quota-update --subnet -1 --network -1 --port -1"
+        logger.info("Setting subnet/net quota to unlimited : %s" % cmd_line)
+        cmd = os.popen(cmd_line)
+        output = cmd.read()
+        logger.debug(output)
 
-    # Workaround for https://jira.opnfv.org/projects/SDNVPN/issues/SDNVPN-115
-    cmd_line = "nova quota-class-update --instances -1 default"
-    logger.info("Setting instances quota to unlimited : %s" % cmd_line)
-    cmd = os.popen(cmd_line)
-    output = cmd.read()
-    logger.debug(output)
+        # Workaround for
+        # https://jira.opnfv.org/projects/SDNVPN/issues/SDNVPN-115
+        cmd_line = "nova quota-class-update --instances -1 default"
+        logger.info("Setting instances quota to unlimited : %s" % cmd_line)
+        cmd = os.popen(cmd_line)
+        output = cmd.read()
+        logger.debug(output)
 
-    with open(COMMON_CONFIG.config_file) as f:
-        config_yaml = yaml.safe_load(f)
+        with open(COMMON_CONFIG.config_file) as f:
+            config_yaml = yaml.safe_load(f)
 
-    testcases = config_yaml.get("testcases")
-    overall_status = "PASS"
-    for testcase in testcases:
-        if testcases[testcase]['enabled']:
-            test_name = testcase
-            test_descr = testcases[testcase]['description']
-            test_name_db = testcases[testcase]['testname_db']
-            title = ("Running '%s - %s'" %
-                     (test_name, test_descr))
-            logger.info(title)
-            logger.info("%s\n" % ("=" * len(title)))
-            t = importlib.import_module(testcase, package=None)
-            start_time = time.time()
-            try:
-                result = t.main()
-            except Exception as ex:
-                result = -1
-                logger.info("Caught Exception in %s: %s Trace: %s" %
-                            (test_name, ex, traceback.format_exc()))
-            end_time = time.time()
-            if result < 0:
-                status = "FAIL"
-                overall_status = "FAIL"
-                logger.info("Testcase %s failed" % test_name)
-            else:
-                status = result.get("status")
-                details = result.get("details")
-                logger.info("Results of test case '%s - %s':\n%s\n" %
-                            (test_name, test_descr, result))
-
-                if status == "FAIL":
+        testcases = config_yaml.get("testcases")
+        overall_status = "PASS"
+        for tc in testcases:
+            if testcases[tc]['enabled']:
+                test_name = tc
+                test_descr = testcases[tc]['description']
+                title = ("Running '%s - %s'" %
+                         (test_name, test_descr))
+                logger.info(title)
+                logger.info("%s\n" % ("=" * len(title)))
+                module = 'sdnvpn.test.functest.' + test_name
+                t = importlib.import_module(module, package=None)
+                try:
+                    result = t.main()
+                except Exception as ex:
+                    result = -1
+                    logger.info("Caught Exception in %s: %s Trace: %s" %
+                                (test_name, ex, traceback.format_exc()))
+                if result < 0:
+                    status = "FAIL"
                     overall_status = "FAIL"
+                    logger.info("Testcase %s failed" % test_name)
+                else:
+                    status = result.get("status")
+                    self.details.update(
+                        {test_name: {'status': status,
+                                     'details': result.get("details")}})
+                    logger.info("Results of test case '%s - %s':\n%s\n" %
+                                (test_name, test_descr, result))
 
-            if report:
-                push_results(
-                    test_name_db, start_time, end_time, status, details)
+                    if status == "FAIL":
+                        overall_status = "FAIL"
 
-    if overall_status == "FAIL":
-        sys.exit(-1)
+        self.stop_time = time.time()
 
-    sys.exit(0)
+        if overall_status == "PASS":
+            self.result = 100
+            return testcase.TestCase.EX_OK
+
+        return testcase.TestCase.EX_RUN_ERROR
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--report",
-                        help="Create json result file",
-                        action="store_true")
-    args = parser.parse_args()
-    main(report=args.report)
+    logger = logging.getLogger('ODL-SFC')
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s'
+                                  '- %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    SDNVPN = SdnvpnFunctest()
+    sys.exit(SDNVPN.run())
