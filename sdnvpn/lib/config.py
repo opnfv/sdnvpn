cf380fceb2ddedefd214e500597c175b6f8af112
Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>
2017-10-10 11:42:39 +0200

    make CommonConfig as singleton
    * It is not necessary to instantiate CommonConfig object from each
    testcase and can be made as singleton
    
    Change-Id: Ibb80848c0d6dcfe26995fb11751ffeeae5d342ee
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>

--- sdnvpn/lib/config.py
+++ sdnvpn/lib/config.py
@@ -21,34 +21,41 @@ class CommonConfig(object):
     """
     Common configuration parameters across testcases
     """
+    class __CommonConfig:
+        def __init__(self):
+            self.config_file = pkg_resources.resource_filename(
+                'sdnvpn', 'test/functest/config.yaml')
+            self.keyfile_path = pkg_resources.resource_filename(
+                'sdnvpn', 'artifacts/id_rsa')
+            self.test_db = CONST.results_test_db_url
+            self.quagga_setup_script_path = pkg_resources.resource_filename(
+                'sdnvpn', 'artifacts/quagga_setup.sh')
+            self.line_length = 90  # length for the summary table
+            self.vm_boot_timeout = 180
+            self.default_flavor = ft_utils.get_parameter_from_yaml(
+                "defaults.flavor", self.config_file)
+            self.image_filename = CONST.openstack_image_file_name
+            self.image_format = CONST.openstack_image_disk_format
+            self.image_path = '{0}/{1}'.format(CONST.dir_functest_images,
+                                               self.image_filename)
+            # This is the ubuntu image used by sfc
+            # Basically vanilla ubuntu + some scripts in there
+            # We can use it to setup a quagga instance
+            # TODO does functest have an ubuntu image somewhere?
+            self.ubuntu_image_name = "sdnvpn-ubuntu"
+            self.ubuntu_image_path = '{0}/{1}'.format(
+                CONST.dir_functest_images,
+                "ubuntu-16.04-server-cloudimg-amd64-disk1.img")
+            self.custom_flavor_name = 'm1.custom'
+            self.custom_flavor_ram = 1024
+            self.custom_flavor_disk = 10
+            self.custom_flavor_vcpus = 1
+    instance = None
     def __init__(self):
-        self.config_file = pkg_resources.resource_filename(
-            'sdnvpn', 'test/functest/config.yaml')
-        self.keyfile_path = pkg_resources.resource_filename(
-            'sdnvpn', 'artifacts/id_rsa')
-        self.test_db = CONST.results_test_db_url
-        self.quagga_setup_script_path = pkg_resources.resource_filename(
-            'sdnvpn', 'artifacts/quagga_setup.sh')
-        self.line_length = 90  # length for the summary table
-        self.vm_boot_timeout = 180
-        self.default_flavor = ft_utils.get_parameter_from_yaml(
-            "defaults.flavor", self.config_file)
-        self.image_filename = CONST.openstack_image_file_name
-        self.image_format = CONST.openstack_image_disk_format
-        self.image_path = '{0}/{1}'.format(CONST.dir_functest_images,
-                                           self.image_filename)
-        # This is the ubuntu image used by sfc
-        # Basically vanilla ubuntu + some scripts in there
-        # We can use it to setup a quagga instance
-        # TODO does functest have an ubuntu image somewhere?
-        self.ubuntu_image_name = "sdnvpn-ubuntu"
-        self.ubuntu_image_path = '{0}/{1}'.format(
-            CONST.dir_functest_images,
-            "ubuntu-16.04-server-cloudimg-amd64-disk1.img")
-        self.custom_flavor_name = 'm1.custom'
-        self.custom_flavor_ram = 1024
-        self.custom_flavor_disk = 10
-        self.custom_flavor_vcpus = 1
+        if not CommonConfig.instance:
+            CommonConfig.instance = CommonConfig.__CommonConfig()
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
 
 
 class TestcaseConfig(object):
@@ -68,3 +75,6 @@ class TestcaseConfig(object):
                          .format(testcase, common_config.config_file))
         # Update class fields with configuration variables dynamically
         self.__dict__.update(**test_config)
+
+x = CommonConfig()
+print(x.custom_flavor_name)
