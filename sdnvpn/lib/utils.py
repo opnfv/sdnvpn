cf6b86658397a6c7c47187d74e21dd049ba5fb9c
Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>
2017-10-19 14:34:19 +0200

    use openstack api's instead of CLIs
    add two apis update_instance_quota_class and
    update_nw_subnet_port_quota and make run_sdnvpn_tests.py to use these
    APIs for setting quotas
    
    JIRA:SDNVPN-188
    
    Change-Id: I764bb6897e6c4c5abad4d252c1f3b4ccc742d52e
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>

--- sdnvpn/lib/utils.py
+++ sdnvpn/lib/utils.py
@@ -681,3 +681,32 @@ def is_fail_mode_secure():
                               'in {} node'.format(openstack_node.name))
                 is_secure[openstack_node.name] = False
     return is_secure
+
+
+def update_nw_subnet_port_quota(neutron_client, tenant_id, nw_quota,
+                                subnet_quota, port_quota):
+    json_body = {"quota": {
+        "network": nw_quota,
+        "subnet": subnet_quota,
+        "port": port_quota
+    }}
+
+    try:
+        neutron_client.update_quota(tenant_id=tenant_id,
+                                    body=json_body)
+        return True
+    except Exception as e:
+        logger.error("Error [update_nw_subnet_port_quota(neutron_client,"
+                     " '%s', '%s', '%s', '%s')]: %s" %
+                     (tenant_id, nw_quota, subnet_quota, port_quota, e))
+        return False
+
+
+def update_instance_quota_class(nova_client, instances_quota):
+    try:
+        nova_client.quota_classes.update("default", instances=instances_quota)
+        return True
+    except Exception as e:
+        logger.error("Error [update_instance_quota_class(nova_client,"
+                     " '%s' )]: %s" % (instances_quota, e))
+        return False
