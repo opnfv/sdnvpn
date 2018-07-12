c5c6780856fa7a53608e8a4b45e00f12b493cab9
nikoskarandreas <nick@intracom-telecom.com>
2018-07-12 19:40:30 +0300

    Using heat orchestrator for sdnvpn - test case 1
    Heat orchestrator and the use of Heat Orchestrator Templates
    is introduced in sdnvpn test cases. The deployment of the
    nodes and networks under test is performed as a stack with the
    use of the heat api and the use of the other apis is kept to
    as little as possible. The scenarios that are executed are the
    same as in the orginal test cases.
    
    This is the implementation of sdnvpn test case 1:
    VPN provides connectivity between subnets
    and also base functions for heat api access and some utilities.
    
    JIRA: SDNVPN-219
    
    Change-Id: Ic284722e600652c9058da96d349dff9398bcacf1
    Signed-off-by: nikoskarandreas <nick@intracom-telecom.com>

--- sdnvpn/lib/openstack_utils.py
+++ sdnvpn/lib/openstack_utils.py
@@ -1505,3 +1505,39 @@ def get_resource(heat_client, stack_id, resource):
     except Exception as e:
         logger.error("Error [get_resource]: %s" % e)
         return None
+
+
+def create_stack(heat_client, **kwargs):
+    try:
+        stack = heat_client.stacks.create(**kwargs)
+        return stack['stack']['id']
+    except Exception as e:
+        logger.error("Error [create_stack]: %s" % e)
+        return None
+
+
+def delete_stack(heat_client, stack_id):
+    try:
+        heat_client.stacks.delete(stack_id)
+        return True
+    except Exception as e:
+        logger.error("Error [delete_stack]: %s" % e)
+        return False
+
+
+def list_stack(heat_client, **kwargs):
+    try:
+        result = heat_client.stacks.list(**kwargs)
+        return result
+    except Exception as e:
+        logger.error("Error [list_stack]: %s" % e)
+        return None
+
+
+def get_output(heat_client, stack_id, output_key):
+    try:
+        output = heat_client.stacks.output_show(stack_id, output_key)
+        return output
+    except Exception as e:
+        logger.error("Error [get_output]: %s" % e)
+        return None
